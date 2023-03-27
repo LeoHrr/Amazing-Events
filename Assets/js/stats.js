e1c1225871b29d7832bf10757099930ae1625045
LeoHrr <leonel177476@gmail.com>
2023-03-27 14:26:20 -0300

    add stats.js

---
+++ Assets/js/stats.js
@@ -0,0 +1,10 @@
+const urlApi = "https://mindhub-xj03.onrender.com/api/amazing"
+let dataArray = []
+
+async function getEvents() {
+    try{
+
+    }catch(e){
+        console.log(e.message);
+    }
+}
